2772fca9891e7a385a6c1c06817a2f6f6d8ba174
Shay Howe <shayhowe@gmail.com>
2013-09-12 14:14:12 -0500

    Updating config

--- config.rb
+++ config.rb
@@ -35,9 +35,6 @@
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
-# Reload the browser automatically whenever files change
-# activate :livereload
-
 # Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
